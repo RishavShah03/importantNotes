b8f8815603e8cde1e9a11a3a3c1b660aed08ad7e
Rishav kumar shah <53249588+Rishu2000@users.noreply.github.com>
2022-01-17 09:56:03 +0530

    fixes added result after action. (#7)
    Co-authored-by: RishavShah03 <rishav@fabstudio.co>

--- 2021/November/deleteProperty.js
+++ 2021/November/deleteProperty.js
@@ -1,7 +1,8 @@
 // The JavaScript delete operator removes a property from an object
 
-const obj = {name:"Rishav",roll:5,class:10}
+const obj = { name: "Rishav", roll: 5, class: 10 };
 
 delete obj.class;
 
 console.log(obj);
+//{name:"Rishav",roll:5}
